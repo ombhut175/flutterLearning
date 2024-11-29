02f81e567a05149b0ae8d3c6c978e541872630ad
om <codersarea12@gmail.com>
2024-11-29 22:00:16 +0530

    lab 1 completed

--- lab1/lib/lab1/hello.dart
+++ lab1/lib/lab1/hello.dart
@@ -1,5 +1,14 @@
 import 'dart:io';
 
 void main(){
-  stdout.writeln("hello world");
+  // print("hello world");
+  stdout.writeln("enter integer");
+  int a = int.parse(stdin.readLineSync()!);
+  stdout.writeln(a);
+  stdout.writeln("enter double");
+  double b = double.parse(stdin.readLineSync()!);
+  stdout.writeln(b);
+  stdout.writeln("enter string");
+  String s = stdin.readLineSync()!;
+  stdout.writeln(s);
 }

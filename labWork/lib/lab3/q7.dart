61c9e2ba2393e53f8d4c856a2189c6ffd92c24ed
om <codersarea12@gmail.com>
2024-12-15 13:44:38 +0530

    new files added

---
+++ labWork/lib/lab3/q7.dart
@@ -0,0 +1,8 @@
+// WAP to find the element that appears only once in a non-empty array of integers, where every
+// other element appears twice. The solution should have linear runtime complexity and use only
+// constant extra space. Example: Input: nums = [2, 2, 1], Output: 1; Input: nums = [4, 1, 2, 1, 2],
+// Output: 4; Input: nums = [1], Output: 1.
+
+void main(){
+    
+}
